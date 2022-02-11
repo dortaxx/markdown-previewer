06806f7c64cc2ce21c64e88ee3969a86f05267c9
dortaxx <akhvledianiluka37@gmail.com>
2022-02-11 18:59:43 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useState, } from 'react';
+import {marked}from 'marked'
 function App() {
+  const [text,setText] = useState('# heading\nparagraph\n# H2\n**BOLDED**\n* list item\n[Visit my webpage](www.com.com)\n*inline`<div><div>`\n```\nlet x = 10 ```')
+
+  const handleChange = (e) =>{
+setText(e.target.value)
+
+  }
+  const markdown = marked(text, {breaks:true})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+ <h1>Markdown Previewer</h1>
+ <div>
+<textarea  className='input' value={text} onChange={handleChange}></textarea>
+ </div>
+ <div className='output' dangerouslySetInnerHTML={{__html:markdown}}></div>
+ </div>
+ 
+
     </div>
   );
 }
